92cc8b03f39c351469bfd6d140f2fbe7d15d1924
Aleksandr <96025122+laleks6@users.noreply.github.com>
2023-04-14 14:50:37 +0300

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(string) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  //let convertNumer = Number(string);
+  //console.log(typeof +convertNumer + convertNumer)
+  //let result
+  if(typeof string !== "string" || isNaN(string) || string.length === 0 || +string > 15 || +string <= 0 ){
+    result = false
+  }else {
+    let a = Math.log2(MODERN_ACTIVITY / +string);
+
+    result = Math.ceil(a * HALF_LIFE_PERIOD )
+  }
+  return result 
 }
 
 module.exports = {
