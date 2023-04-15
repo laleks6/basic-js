0c821879285965436cf9971f53f3f245df9acfb3
Aleksandr <96025122+laleks6@users.noreply.github.com>
2023-04-15 20:18:26 +0300

    feat: unfinished day 2

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let resultArr = arr
+  if(!Array.isArray(arr)) {
+    
+  }else if(resultArr.length < 1 ){
+
+  }
+  else {
+    for(let i = 0; i < resultArr.length; i++) {
+   
+      if(resultArr[i] === "--discard-next") {
+        if(i === 0 ) {
+          
+          resultArr.splice(i, 2)
+         
+        }else {
+          resultArr.splice(i, 2)
+          transform(resultArr)
+        
+        }
+
+      }else if(resultArr[i] === "--discard-prev") {
+        if(i === 0 ) {
+          
+          resultArr.splice(i, 2)
+        }else {
+          resultArr.splice(i, 2)
+          transform(resultArr)
+        
+        }
+        
+      }else if(resultArr[i] === "--double-next") {
+        if(i === resultArr.length - 1  ) {
+         
+          resultArr.splice(i, 0, resultArr[i+1])
+        }else {
+          resultArr.splice(i, 1, resultArr[i+1])
+          transform(resultArr)
+
+        } 
+       
+       
+      }else if(resultArr[i] === "--double-prev") {
+        if(i === 0 ) {
+          resultArr.splice(i, 0, resultArr[i-1])
+        }
+        }else {
+          resultArr.splice(i, 1, resultArr[i-1])
+          transform(resultArr)
+        }
+      
+         
+       
+      
+    }
+  }
+  return resultArr
 }
 
 module.exports = {
