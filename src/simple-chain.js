7005fb3dbeb439f38ac09cf45cb0a65231d2474d
Aleksandr <96025122+laleks6@users.noreply.github.com>
2023-04-16 19:47:11 +0300

    feat: unfinished 3

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,26 +4,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
+
 const chainMaker = {
+  result: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.result.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+   // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if(value === " "){
+      this.result.push(" ")
+    }else if(typeof value === 'number'){
+      this.result.push(`${value}`)
+    }
+    else{
+      
+     
+    }
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position ) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if(position !== "number" || position % 1 !== 0 || position < 0 || position > this.result.length){
+      throw new Error("You can't remove incorrect link!");
+    }
+    
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.result.reverse()
+    return this
+    
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    let chain = this.result.join("~~")
+    this.result = []
+    return chain
   }
 };
 
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,64 +13,78 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(arr) {
+
+ function transform(arr) {
   //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-  let resultArr = arr
+  let resultArr 
   if(!Array.isArray(arr)) {
-    
+    resultArr = Array.isArray(arr)
   }else if(resultArr.length < 1 ){
 
-  }
-  else {
+  }else {
     for(let i = 0; i < resultArr.length; i++) {
-   
+    console.log(resultArr[i])
+      console.log(resultArr)
       if(resultArr[i] === "--discard-next") {
         if(i === 0 ) {
-          
-          resultArr.splice(i, 2)
-         
+          resultArr.shift()
+          transform(resultArr)
         }else {
+          console.log("proverka next" )
           resultArr.splice(i, 2)
           transform(resultArr)
-        
         }
-
       }else if(resultArr[i] === "--discard-prev") {
         if(i === 0 ) {
           
-          resultArr.splice(i, 2)
-        }else {
-          resultArr.splice(i, 2)
+          resultArr.shift()
           transform(resultArr)
-        
+        }else if (resultArr[i-2] === '--discard-next'){
+          console.log(resultArr[i-1] === arr[i-1])
+          console.log("prev dub 2")
+          resultArr.splice(i, 1)
+          transform(resultArr)
+        }else {
+          resultArr.splice(i-1, 2)
+          transform(resultArr)
         }
-        
       }else if(resultArr[i] === "--double-next") {
         if(i === resultArr.length - 1  ) {
-         
-          resultArr.splice(i, 0, resultArr[i+1])
+          resultArr.pop()
+          transform(resultArr)
+        }else if (resultArr[i-2] === '--discard-next'){
+          console.log(resultArr[i-1] === arr[i-1])
+          console.log("prev dub 2")
+          resultArr.splice(i, 1)
+          transform(resultArr)
         }else {
           resultArr.splice(i, 1, resultArr[i+1])
           transform(resultArr)
-
         } 
        
        
       }else if(resultArr[i] === "--double-prev") {
         if(i === 0 ) {
-          resultArr.splice(i, 0, resultArr[i-1])
-        }
-        }else {
-          resultArr.splice(i, 1, resultArr[i-1])
+          console.log("prev dub 1")
+          resultArr.shift()
           transform(resultArr)
+        }else if (resultArr[i-2] === '--discard-next'){
+          console.log(resultArr[i-1] === arr[i-1])
+          console.log("prev dub 2")
+          resultArr.splice(i, 1)
+          transform(resultArr)
+        }else {
+           console.log(resultArr[i-1] === arr[i-1])
+        console.log("prev dub 3")
+        resultArr.splice(i, 1, resultArr[i-1])
+          transform(resultArr)
+     
         }
-      
-         
-       
-      
+      }
     }
   }
+  console.log(...resultArr)
   return resultArr
 }
 
