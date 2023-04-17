eb9bce10bd06caa92f10b9fd4f49d6e07b759411
Aleksandr <96025122+laleks6@users.noreply.github.com>
2023-04-17 12:13:37 +0300

    feat: chain simple

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -18,21 +18,21 @@ const chainMaker = {
     // remove line with error and write your code here
     if(value === " "){
       this.result.push(" ")
-    }else if(typeof value === 'number'){
-      this.result.push(`${value}`)
+    }else{
+      this.result.push(`( ${value} )`)
     }
-    else{
-      
-     
-    }
+    
     return this
   },
-  removeLink(position ) {
+  removeLink(position) {
     //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-    if(position !== "number" || position % 1 !== 0 || position < 0 || position > this.result.length){
+    if(typeof position !== "number" || position % 1 !== 0 || position <= 0 || position > this.getLength()){
+      this.result = []
       throw new Error("You can't remove incorrect link!");
     }
+    this.result.splice(position - 1, 1)
+    return this
     
   },
   reverseChain() {
